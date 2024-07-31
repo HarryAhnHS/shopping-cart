311908897169b6eaf20d315608a93834bb6f04fe
HarryAhnHS <hongsuhahn@gmail.com>
2024-07-31 17:30:08 +0800

    chore: placeholder styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="py-5 bg-red-800 text-white">
-            <Link to="/" className="mx-1">Home</Link>
-            <Link to="Shop" className="mx-1">Shop</Link>
+        <div>
+            <div className="flex py-5 bg-red-800 text-white">
+                <Link to="/" className="mx-1">Home</Link>
+                <Link to="/Shop" className="mx-1">Shop</Link>
+                <button className="mx-1">Cart</button>
+            </div>
         </div>
+
     )
 }
 
