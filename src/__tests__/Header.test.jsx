65e67aa5f6f909b4a3c028f345abb0238c64431d
HarryAhnHS <hongsuhahn@gmail.com>
2024-07-31 22:12:51 +0800

    test: vitest and react-testing-library setup

---
+++ src/__tests__/Header.test.jsx
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import Shop from '../Shop'
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Header', () => {
+  it('renders home, shop, and cart links', () => {
+    render(
+      <BrowserRouter>
+        <Shop />
+      </BrowserRouter>
+    );
+    const homeLink = screen.getByRole("link", { name: /Home/i });
+    const shopLink = screen.getByRole("link", { name: /Shop/i });
+    const cart = screen.getByRole("button", { name: /Cart/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(homeLink).toHaveAttribute("href");
+    expect(shopLink).toBeInTheDocument();
+    expect(shopLink).toHaveAttribute("href");
+    expect(cart).toBeInTheDocument();
+  });
+});
