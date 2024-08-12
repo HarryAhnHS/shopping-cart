7ae9ff6306093f028592762b99cbdf43960bf79b
HarryAhnHS <hongsuhahn@gmail.com>
2024-08-12 08:11:02 +0100

    style: responsive text

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -22,22 +22,22 @@ const Shop = ({products, isCalled, decreaseSelected, handleSelectedChange, incre
                     <div className="flex flex-wrap p-6">
                         {products.map((prod) => {
                             return (
-                                    <div className="box-border w-full sm:w-1/2 lg:w-1/3 p-3" key={prod.id}>
+                                    <div className="box-border w-full md:w-1/2 lg:w-1/3 p-3" key={prod.id}>
                                         <div className="flex rounded-lg cursor-pointer bg-[#FFFFFF] shadow-xl" onClick={() => routeChange(prod.id)}>
                                             <div className="flex-none w-1/3 h-64">
                                                 <img src={prod.images[0]} className="inset-0 w-full h-full object-cover rounded-l-lg"></img>
                                             </div>
                                             <div className="flex-auto p-6 flex flex-col">
                                                 <div className="flex flex-col">
-                                                    <h1 className="flex-auto text-lg font-semibold text-slate-9000">{prod.title}</h1>
-                                                    <div className="text-lg font-semibold text-slate-500">${prod.price}</div>
+                                                    <h1 className="flex-auto text-base md:text-lg font-semibold text-slate-9000">{prod.title}</h1>
+                                                    <div className="text-base md:text-lg font-semibold text-slate-500">${prod.price}</div>
                                                 </div>
                                                 <div className="flex flex-1 items-center justify-center mb-5">
                                                     <button className="w-8 h-8 border-2" onClick={(e) => decreaseSelected(e, prod.id)}>-</button>
                                                     <input className="w-8 h-8 text-center" type="text" min="0" value={prod.selected} onClick={(e) => e.stopPropagation()} onChange={(e) => handleSelectedChange(e, prod.id)}></input>
                                                     <button className="w-8 h-8 border-2" onClick={(e) => increaseSelected(e, prod.id)}>+</button>
                                                 </div>
-                                                <button className="h-10 px-12 font-semibold text-[#FFFFFF] bg-[#283618] active:scale-95 transition-all duration-100" type="submit" onClick={(e) => addToCart(e, prod.id)}>
+                                                <button className="h-10 text-[0.9rem] font-semibold text-[#FFFFFF] bg-[#283618] active:scale-95 transition-all duration-100" type="submit" onClick={(e) => addToCart(e, prod.id)}>
                                                         Add to cart
                                                 </button>
                                             </div>
